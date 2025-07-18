169cc57e6003da33aa8e77a91294c45d71d701e9
Jai Goyal <jaigoel14@gmail.com>
2025-07-18 16:04:22 +0530

    new folder(Jai)

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,3 @@
-
-
 // External Module
 const express = require("express");
 const mongoose = require("mongoose");
@@ -11,9 +9,7 @@ const cors = require("cors");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-mongoose.connect(MONGO_DB_URL).then(() => {
   app.listen(PORT, () => {
     console.log('Hello');
     console.log(`Server running at: http://localhost:${PORT}`);
   });
-});
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,8 @@ import React from "react";
 
 function App() {
   return (
-    <BrowserRouter>
       <div>Hello World</div>
-    </BrowserRouter>
+    
   );
 }
 
